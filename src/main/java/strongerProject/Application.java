3afdfc54f8a464ebf743662886ed3ebcc343349a
devcloudStronger <114921409@qq.com>
2018-03-06 20:50:08 +0800

    sssss

--- src/main/java/strongerProject/Application.java
+++ src/main/java/strongerProject/Application.java
@@ -1,5 +1,20 @@
 package strongerProject;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
 }
